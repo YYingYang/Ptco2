2aecfef1c1e7262ed880284bd424cfb8a4c03711
Giacomo Yang <giacomo.gy@gmail.com>
2021-06-21 16:41:43 +0200

    fix: small change

--- src/views/components/footer/Footer.js
+++ src/views/components/footer/Footer.js
@@ -1,23 +1,14 @@
 class Footer {
   static render() {
-    return `<footer>
-    <div
-      class="bg-image hover-overlay ripple shadow-1-strong rounded"
-      data-ripple-color="light"
-    >
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/9/98/Engineering_logo.png"
-        class="w-100"
-      />
+    return `<footer class="text-center text-white" style="background-color: #f1f1f1;">
+    <div class="container pt-4">
+        <img class="englogo" src = "./src/assets/eng.png">
     </div>
-    <div
-      class="text-center p-4"
-      style="background-color: rgba(0, 0, 0, 0.05)"
-    >
-      © 2021 Copyright:
-      <a class="text-reset fw-bold">Engineering.com</a>
+    <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2021 Copyright:
+        <a class="text-dark" href="https://www.eng.it/%22%3EEngineering S.p.A.</a>
     </div>
-  </footer>`;
+</footer>`;
   }
 }
 
