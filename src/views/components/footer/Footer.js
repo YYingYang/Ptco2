2686800b628c4b103d0c508db55fcb03312cc164
Giacomo Yang <giacomo.gy@gmail.com>
2021-06-28 16:13:50 +0200

    fix: cahnge logo class

--- src/views/components/footer/Footer.js
+++ src/views/components/footer/Footer.js
@@ -2,7 +2,7 @@ class Footer {
   render() {
     return `<footer class="text-center text-white" style="background-color: #f1f1f1;">
     <div class="container pt-4">
-        <img class="englogo" src = "./src/assets/eng.png">
+        <img src = "./src/assets/eng.png">
     </div>
     <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
         © 2021 Copyright:
