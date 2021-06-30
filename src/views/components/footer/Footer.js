1dea1b1f339b804a035c2a540b19cfb40fdc2e7f
Giacomo Yang <giacomo.gy@gmail.com>
2021-06-30 09:17:06 +0200

    fix: move img logo

--- src/views/components/footer/Footer.js
+++ src/views/components/footer/Footer.js
@@ -1,11 +1,8 @@
 class Footer {
   render() {
     return `<footer class="text-center text-white" style="background-color: #f1f1f1;">
-    <div class="container pt-4">
-        <img src = "./src/assets/eng.png">
-    </div>
     <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        © 2021 Copyright:
+        © 2021 Copyright: <img src = "./src/assets/eng.png">
         <a class="text-dark" href="https://www.eng.it/%22%3EEngineering S.p.A.</a>
     </div>
 </footer>`;
