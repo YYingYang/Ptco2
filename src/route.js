2416a99afb3c74e9f04affaadd1ad322ba34b5b2
Giacomo Yang <giacomo.gy@gmail.com>
2021-07-01 10:13:30 +0200

    fix: add route

---
+++ src/route.js
@@ -0,0 +1,9 @@
+import Details from "./views/components/details/Details";
+import Home from "./views/components/home/Home";
+
+const Route = new Map([
+  ['', Home],
+  ['details', Details]
+]);
+
+export default Route;
